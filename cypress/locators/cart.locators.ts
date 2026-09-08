1a4cae66d21cd25fea1e07980df053bbdaaa889d
ake <ake@quinta.im>
2026-09-08 10:52:02 +0100

    feat: add additional locators for cart page elements

--- cypress/locators/cart.locators.ts
+++ cypress/locators/cart.locators.ts
@@ -4,6 +4,11 @@
  */
 const cartLocators = {
   cartItemName: "[data-test='inventory-item-name']",
+  buttonRemove: "[data-test='remove-']",
+  buttonCheckout:"[data-test='checkout']",
+  buttoncontinueShopping:"[data-test='continue-shopping']",
+  iconeshoppingCartLink: "[data-test='shopping-cart-link']",
+  iconeshoppingCartBadge: "[data-test='shopping-cart-badge']",
 } as const;
 
 export default cartLocators;
